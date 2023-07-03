950f64cf1eb7d5a4e3f2c65af1ef566a7f16d5c6
Ankita Sankpal <106144681+Ankita-Sankpal@users.noreply.github.com>
2023-07-03 22:55:30 +0530

    second.py

--- second.py
+++ second.py
@@ -1,7 +1,31 @@
-import random
-import string
+import random  
+  
+def password_generator(char_length, num_length, symbol_length):  
 
-# get random password pf length 8 with letters, digits, and symbols
-characters = string.ascii_letters + string.digits + string.punctuation
-password = ''.join(random.choice(characters) for i in range(8))
-print("Random password is:", password)
+  
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"  
+    nums = "0123456789"  
+    symbols = "!@#$%^&*()"  
+  
+  
+    selectedChars = random.sample(chars, char_length)  
+    selectedNums = random.sample(nums, num_length)  
+    selectedSymbols = random.sample(symbols, symbol_length)  
+  
+    password_list = selectedChars + selectedNums + selectedSymbols  
+  
+    random.shuffle(password_list)  
+  
+    password_str = "".join(password_list)  
+  
+    return password_str  
+  
+if __name__ == "__main__":  
+    char_length = int(input("Enter the Number of the Characters that you need in the password : "))  
+    num_length = int(input("Enter the Number of the Numbers that you need in the password : "))  
+    symbol_length = int(input("Enter the Number of the Symbols that you need in the password : "))  
+  
+ 
+    password = password_generator(char_length, num_length, symbol_length)  
+  
+    print("The Randomly Generated Password is :", password) 
